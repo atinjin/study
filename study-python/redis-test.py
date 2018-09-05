1b6408f0983fb57acfa6a39e311e37e535211bee
진동현(아틴/Atin) <atinjin@coupang.com>
2018-09-05 20:26:38 +0900

    update

--- study-python/redis-test.py
+++ study-python/redis-test.py
@@ -7,7 +7,32 @@ r = redis.Redis(
 
 r.set('test-key', 'test-value')
 value = r.get('test-key')
+value = r.get('test-key')
 print(value)
 
 df = pd.read_excel('PriceTrend.xlsx')
-print(df)
+print(df.info())
+
+date = df.loc[1,"Date"].date()
+price = int(df.loc[1,"SU 1"])
+print("su1= {:d}".format(price))
+
+diHashFormat = "DI:VI#{viId}:{year}:{month}:{day}"
+viId = "540101022"
+year = date.year
+month = date.month
+day =  date.day
+diKey = diHashFormat.format(viId=viId, year=year, month=month, day=day)
+print(diKey)
+
+ddHashFormat = "SU:{rateId}#DD:{wf}:{wb}#RANK:{rank}"
+rateId = 101
+wf = wb = 7
+rank = 1
+suKey = ddHashFormat.format(rateId=rateId, wf=wf, wb=wb, rank=rank)
+
+print(suKey)
+
+r.hmset(diKey, {suKey: price})
+
+print(r.hget(diKey, suKey).decode())
